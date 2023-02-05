00c57a8a3ac1a7d70b1b3ec74f65c8897119eca5
David Isidore <isidoredavid@protonmail.com>
2023-02-05 18:18:01 +0100

    Ajout du retour dans l'historique

--- src/index.js
+++ src/index.js
@@ -90,6 +90,18 @@ import './index.css'
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {      
+        const desc = move ?        
+        'Revenir au tour n°' + move :        
+        'Revenir au début de la partie';      
+        return (        
+        <li>          
+        <button onClick={() => this.jumpTo(move)}>{desc}</button>        
+        </li>      
+        );    
+    });
+
   
       let status;
       if (winner) {
@@ -108,7 +120,7 @@ import './index.css'
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{ moves }</ol>
           </div>
         </div>
       );
